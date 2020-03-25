a1ca0f128e9ea1e81b953d07c40f058bbcb8272c
elsbrd <56909624+elsbrd@users.noreply.github.com>
2020-03-25 21:51:15 +0200

    commit 3

--- Backup/Backup/Backup/Class1.cs
+++ Backup/Backup/Backup/Class1.cs
@@ -2,19 +2,19 @@ using System;
 
 namespace TextHello
 {
-	/// <summary>
-	/// The obligatory Hello World in C#
-	/// </summary>
-	/// <remarks>
-	/// This program writes out Hello World 
-	/// using the WriteLine method of the
-	/// System.Console class.
-	/// </remarks>
-	class Greeting
-	{
-		static void Main(string[] args)
-		{
-			Console.WriteLine("Input the arithmetic expression:");
+    /// <summary>
+    /// The obligatory Hello World in C#
+    /// </summary>
+    /// <remarks>
+    /// This program writes out Hello World 
+    /// using the WriteLine method of the
+    /// System.Console class.
+    /// </remarks>
+    class Greeting
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input the arithmetic expression:");
             string s = "";
             // To check the length of  
             // Command line arguments   
@@ -38,6 +38,7 @@ namespace TextHello
         public static List<string> numbers = new List<string>() { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "," };
         public static List<string> operations = new List<string>() { "^", "/", "*", "+", "-" };
         public static List<string> skobki = new List<string>() { "(", ")" };
+
 
 
         public static double CalculateExpression(string priklad)
@@ -108,8 +109,116 @@ namespace TextHello
                 symbols.Add(lastSymbol);
                 lastSymbol = "";
             }
-            //////////////////////////////////////////////гдеяэ/////////////////////////////////////////
-            return 0;
+            symbols = Skobki_dop_zadanie(symbols);
+            symbols = Easier_to_calculate(symbols);
+
+            List<string> result = new List<string>();
+
+            string[] temp = symbols[0].Split(' ');
+            for (int i = 0; i < temp.Length; i++)
+            {
+                if (operations.Contains(temp[i]))
+                {
+                    if (temp[i] == "^")
+                    {
+                        result[result.Count - 2] = Math.Pow(double.Parse(result[result.Count - 2]), double.Parse(result[result.Count - 1])).ToString();
+                        result.RemoveRange(result.Count - 1, 1);
+                    }
+                    if (temp[i] == "+")
+                    {
+                        result[result.Count - 2] = (double.Parse(result[result.Count - 2]) + double.Parse(result[result.Count - 1])).ToString();
+                        result.RemoveRange(result.Count - 1, 1);
+                    }
+                    if (temp[i] == "-")
+                    {
+                        result[result.Count - 2] = (double.Parse(result[result.Count - 2]) - double.Parse(result[result.Count - 1])).ToString();
+                        result.RemoveRange(result.Count - 1, 1);
+                    }
+                    if (temp[i] == "*")
+                    {
+                        result[result.Count - 2] = (double.Parse(result[result.Count - 2]) * double.Parse(result[result.Count - 1])).ToString();
+                        result.RemoveRange(result.Count - 1, 1);
+                    }
+                    if (temp[i] == "/")
+                    {
+                        result[result.Count - 2] = (double.Parse(result[result.Count - 2]) / double.Parse(result[result.Count - 1])).ToString();
+                        result.RemoveRange(result.Count - 1, 1);
+                    }
+                }
+
+                else result.Add(temp[i]);
+            }
+
+            return double.Parse(result[0]);
+
         }
+
+        static List<string> Skobki_dop_zadanie(List<string> symbols)
+        {
+            while (symbols.Contains("("))
+            {
+                int start_skobki = 0;
+                int end_skobki = 0;
+                int how_many_in_skob = 0;
+
+                for (int i = 0; i < symbols.Count; i++)
+                {
+                    if (symbols[i] == "(")
+                    {
+                        start_skobki = i;
+                        how_many_in_skob = 1;
+                        break;
+                    }
+                }
+
+                for (int i = start_skobki + 1; i < symbols.Count; i++)
+                {
+                    if (symbols[i] == "(") how_many_in_skob++;
+                    if (symbols[i] == ")") how_many_in_skob--;
+                    if (how_many_in_skob == 0)
+                    {
+                        end_skobki = i;
+                        break;
+                    }
+                }
+
+                string what_in_skobkah = "";
+                for (int i = start_skobki + 1; i < end_skobki; i++) what_in_skobkah += symbols[i];
+
+                symbols[start_skobki] = CalculateExpression(what_in_skobkah).ToString();
+                symbols.RemoveRange(start_skobki + 1, end_skobki - start_skobki);
+            }
+            return symbols;
+        }
+        static List<string> Easier_to_calculate(List<string> symbols)
+        {
+
+            foreach (var j in operations)
+            {
+                var flagO = true;
+                while (flagO)
+                {
+                    flagO = false;
+                    for (int i = 0; i < symbols.Count; i++)
+                    {
+                        if (symbols[i] == j)
+                        {
+                            symbols[i - 1] = symbols[i - 1] + " " + symbols[i + 1] + " " + j;
+                            symbols.RemoveRange(i, 2);
+
+                            flagO = true;
+                            break;
+                        }
+                    }
+                }
+            }
+            return symbols;
+        }
+
+
+
+
     }
 }
+
+
